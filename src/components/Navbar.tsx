b7ec7b3f1cdcbed18cb1e0caad3bfdb6e729c30c
Nurlianto Aldi <nurliantoaldi@gmail.com>
2025-10-25 08:42:21 +0700

    add burger button as responsive features

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,12 @@
 import { NavLink } from "react-router-dom"
 import { navLinks } from "../data/navLinks"
+import { useState } from "react";
+import { Icon } from "@iconify/react";
 
 const Navbar = () => {
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <>
       <nav
@@ -25,6 +30,38 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
+
+        <div className="md:hidden ml-auto">
+        <button onClick={() => setIsMenuOpen(!isMenuOpen)} aria-label="Open menu">
+          <Icon icon="ci:hamburger-lg" width={30} />
+        </button>
+      </div>
+
+      {isMenuOpen && (
+        <div className="md:hidden fixed top-0 right-0 h-screen w-3/4 bg-white z-50 shadow-2xl flex flex-col p-8">
+          <div className="flex justify-end mb-12">
+            <button onClick={() => setIsMenuOpen(false)} aria-label="Close menu">
+              <Icon icon="ci:close-big" width={30} />
+            </button>
+          </div>
+
+          <ul className="flex flex-col items-center gap-10 text-2xl">
+            {navLinks.map((link) => (
+              <li key={link.href}>
+                <NavLink
+                  to={link.href}
+                  className={({ isActive }) =>
+                    `cursor-pointer ${isActive ? "font-bold underline" : ""}`
+                  }
+                  onClick={() => setIsMenuOpen(false)}
+                >
+                  {link.label}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
       </nav>
     </>
   )
