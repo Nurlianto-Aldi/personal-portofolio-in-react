1a202e528ebcf864655659b1636d2af59defc164
Nurlianto Aldi <nurliantoaldi@gmail.com>
2025-10-27 16:02:21 +0700

    hiding DARK MODE

--- src/data/navLinks.ts
+++ src/data/navLinks.ts
@@ -1,5 +1,5 @@
 export const navLinks = [
   { href: "/", label: "ABOUT" },
   { href: "/project", label: "PROJECT" },
-  { href: "/dark-mode", label: "DARK MODE" },
+  // { href: "/dark-mode", label: "DARK MODE" },
 ]
